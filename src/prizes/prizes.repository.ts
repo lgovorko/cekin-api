31947d731a203eb586f75816b46c1f94e2e9d97c
antoniopreucil93 <antonio.preucil@protonmail.com>
2021-11-23 19:50:30 +0100

    fix

--- src/prizes/prizes.repository.ts
+++ src/prizes/prizes.repository.ts
@@ -6,63 +6,63 @@ import { PrizeStatusE } from './enum';
 
 @EntityRepository(Prize)
 export class PrizeRepository extends Repository<Prize> {
-  public async getManyPrizes({
-    page,
-    limit,
-  }: {
-    page: number;
-    limit: number;
-  }): Promise<PrizeCustomResponseDTO> {
-    const pageInt: number = page < 1 ? 1 : page;
-    const limitInt: number = +limit;
+	public async getManyPrizes({
+		page,
+		limit,
+	}: {
+		page: number;
+		limit: number;
+	}): Promise<PrizeCustomResponseDTO> {
+		const pageInt: number = page < 1 ? 1 : page;
+		const limitInt: number = +limit;
 
-    const skipped: number = (pageInt - 1) * limitInt;
+		const skipped: number = (pageInt - 1) * limitInt;
 
-    const [data, total] = await this.createQueryBuilder('prize')
-      .where(`prize.status = ${PrizeStatusE.ACTIVE}`)
-      .skip(skipped)
-      .take(limit)
-      .orderBy('prize.id', 'ASC')
-      .getManyAndCount();
+		const [data, total] = await this.createQueryBuilder('prize')
+			.where(`prize.status = ${PrizeStatusE.ACTIVE}`)
+			.skip(skipped)
+			.take(limit)
+			.orderBy('prize.id', 'ASC')
+			.getManyAndCount();
 
-    const pageCount = Math.ceil(total / limitInt);
+		const pageCount = Math.ceil(total / limitInt);
 
-    return {
-      data,
-      count: limitInt,
-      total,
-      page: pageInt,
-      pageCount,
-    };
-  }
+		return {
+			data,
+			count: limitInt,
+			total,
+			page: pageInt,
+			pageCount,
+		};
+	}
 
-  public async getPrizeDrawsAndResults(prizeId: number): Promise<Prize | []> {
-    const prize = await this.createQueryBuilder('prize')
-      .leftJoinAndSelect('prize.dailyDraws', 'dailyDraws')
-      .leftJoinAndSelect('dailyDraws.drawWinner', 'drawWinner')
-      .leftJoinAndSelect('drawWinner.user', 'user')
-      .where(`prize.id=:prizeId`, { prizeId })
-      .orderBy('dailyDraws.drawDate', 'ASC')
-      .getOne();
+	public async getPrizeDrawsAndResults(prizeId: number): Promise<Prize | []> {
+		const prize = await this.createQueryBuilder('prize')
+			.leftJoinAndSelect('prize.dailyDraws', 'dailyDraws')
+			.leftJoinAndSelect('dailyDraws.drawWinner', 'drawWinner')
+			.leftJoinAndSelect('drawWinner.user', 'user')
+			.where(`prize.id=:prizeId`, { prizeId })
+			.orderBy('dailyDraws.drawDate', 'ASC')
+			.getOne();
+		console.log(prize, ' prize');
+		if (!prize) return [];
 
-    if (!prize) return [];
+		const { dailyDraws } = prize;
+		console.log(dailyDraws, ' dd');
+		const filteredRejectedWinners = dailyDraws.map(currentDailyDraw => {
+			const { drawWinner } = currentDailyDraw;
 
-    const { dailyDraws } = prize;
+			const filteredDrawWinners = drawWinner.filter(
+				currentDrawWinner =>
+					currentDrawWinner.status === DrawWinnerStatusE.CONFIRMED
+			);
 
-    const filteredRejectedWinners = dailyDraws.map(currentDailyDraw => {
-      const { drawWinner } = currentDailyDraw;
+			currentDailyDraw.drawWinner = filteredDrawWinners;
+			return currentDailyDraw;
+		});
 
-      const filteredDrawWinners = drawWinner.filter(
-        currentDrawWinner =>
-          currentDrawWinner.status === DrawWinnerStatusE.CONFIRMED,
-      );
+		prize.dailyDraws = filteredRejectedWinners;
 
-      currentDailyDraw.drawWinner = filteredDrawWinners;
-      return currentDailyDraw;
-    });
-
-    prize.dailyDraws = filteredRejectedWinners;
-
-    return prize;
-  }
+		return prize;
+	}
 }
