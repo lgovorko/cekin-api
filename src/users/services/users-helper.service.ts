d2240fbdc72b6042e402dac640b7d6fa6ebddd9b
antoniopreucil93 <antonio.preucil@protonmail.com>
2021-11-23 19:24:17 +0100

    fix

--- src/users/services/users-helper.service.ts
+++ src/users/services/users-helper.service.ts
@@ -4,69 +4,69 @@ import * as sgTransport from 'nodemailer-sendgrid-transport';
 import { errorMessage } from '../../shared/error-messages/error-messages';
 
 export interface MailI {
-  from?: string;
-  to?: string;
-  subject?: string;
-  text?: string;
-  html?: any;
+	from?: string;
+	to?: string;
+	subject?: string;
+	text?: string;
+	html?: any;
 }
 
 @Injectable()
 export class UserHelperService {
-  private transporter: Transporter;
+	private transporter: Transporter;
 
-  constructor() {
-    this.transporter = createTransport(
-      sgTransport({
-        secure: true,
-        auth: {
-          api_key: process.env.SENDGRID_API_KEY,
-        },
-      }),
-    );
-  }
+	constructor() {
+		this.transporter = createTransport(
+			sgTransport({
+				secure: true,
+				auth: {
+					api_key: process.env.SENDGRID_API_KEY,
+				},
+			})
+		);
+	}
 
-  public async sendVerificationMail(emailPayload: MailI): Promise<void> {
-    const { to, text, subject } = emailPayload;
-  
-    const sent = await this.transporter.sendMail({
-      from: 'noreply@zdraviodabir.hr',
-      to,
-      subject,
-      text,
-    });
-  
-    const { message } = sent;
+	public async sendVerificationMail(emailPayload: MailI): Promise<void> {
+		const { to, text, subject } = emailPayload;
 
-    if (message !== 'success')
-      throw new BadRequestException(errorMessage.mailNotSent);
-  }
+		const sent = await this.transporter.sendMail({
+			from: 'noreply@cekin.hr',
+			to,
+			subject,
+			text,
+		});
 
-  public async sendMailWithTemplate(emailPayload: MailI): Promise<void> {
-    const { to, subject, html } = emailPayload;
+		const { message } = sent;
 
-    const sent = await this.transporter.sendMail({
-      from: 'noreply@zdraviodabir.hr',
-      to,
-      subject: `${subject}`,
-      html,
-    });
-  
-    const { message } = sent;
+		if (message !== 'success')
+			throw new BadRequestException(errorMessage.mailNotSent);
+	}
 
-    if (message !== 'success')
-      throw new BadRequestException(errorMessage.mailNotSent);
-  }
+	public async sendMailWithTemplate(emailPayload: MailI): Promise<void> {
+		const { to, subject, html } = emailPayload;
 
-  generateVerifyUrl(token: string): string {
-    return `${process.env.BASE_API_URL}/users/mail-verification?bearer=${token}`;
-  }
+		const sent = await this.transporter.sendMail({
+			from: 'noreply@cekin.hr',
+			to,
+			subject: `${subject}`,
+			html,
+		});
 
-  renameObjectKey(object: any, key: string, newKey: string): any {
-    const clonedObject = Object.assign({}, object);
-    const targetKey = clonedObject[key];
-    delete clonedObject[key];
-    clonedObject[newKey] = targetKey;
-    return clonedObject;
-  }
+		const { message } = sent;
+
+		if (message !== 'success')
+			throw new BadRequestException(errorMessage.mailNotSent);
+	}
+
+	generateVerifyUrl(token: string): string {
+		return `${process.env.BASE_API_URL}/users/mail-verification?bearer=${token}`;
+	}
+
+	renameObjectKey(object: any, key: string, newKey: string): any {
+		const clonedObject = Object.assign({}, object);
+		const targetKey = clonedObject[key];
+		delete clonedObject[key];
+		clonedObject[newKey] = targetKey;
+		return clonedObject;
+	}
 }
